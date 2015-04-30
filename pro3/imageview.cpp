b792fb30dd85d5f5783b45e3660bcc6e54f8c558
Kage Shen <kkshen@ingenic.cn>
2015-04-30 18:56:24 +0800

    imageview / patch2

--- pro3/imageview.cpp
+++ pro3/imageview.cpp
@@ -21,3 +21,48 @@ ImageView::ImageView()
     resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
 
 }
+
+void ImageView::createActions()
+{
+    openAct = new QAction(tr("&Open..."), this);
+    openAct->setShortcut(tr("Ctrl+0"));
+    connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
+
+    printAct = new QAction(tr("&Print..."), this);
+    printAct->setShortcut(tr("Ctrl+P"));
+    printAct->setEnabled(false);
+    connect(printAct, SIGNAL(triggered()), this, SLOT(print()));
+
+    exitAct = new QAction(tr("E&xit"), this);
+    exitAct->setShortcut(tr("Ctrl+Q"));
+    connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
+
+    zoomInAct = new QAction(tr("Zoom &In (25%)"), this);
+    zoomInAct->setShortcut(tr("Ctrl++"));
+    zoomInAct->setEnabled(false);
+    connect(zoomInAct, SIGNAL(triggered()), this, SLOT(zoomIn()));
+
+    zoomOutAct = new QAction(tr("Zoom &Out (25%)"), this);
+    zoomOutAct->setShortcut(tr("Ctrl+-"));
+    zoomOutAct->setEnabled(false);
+    connect(zoomOutAct, SIGNAL(triggered()), this, SLOT(zoomOut()));
+
+    normalSizeAct = new QAction(tr("&Normal Size"), this);
+    normalSizeAct->setshortcut(tr("Ctrl+S"));
+    normalSizeAct->setEnabled(false);
+    connect(normalSizeAct, SIGNAL(triggered()), this, SLOT(normalSize()));
+
+    fitToWindowAct = new QAction(tr("&Fit to Window"), this);
+    fitToWindowAct->setShortcut(tr("Ctrl+F"));
+    fitToWindowAct->setEnabled(false);
+    fitToWindowAct->setCheckable(true);
+    connect(fitToWindowAct, SIGNAL(triggered()), this, SLOT(fitToWindow()));
+
+    aboutAct = new QAction(tr("&About"), this);
+    connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
+
+    aboutQtAct = new QAction(tr("About &Qt"), this);
+    connect(aboutQtActm, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+}
+
+
